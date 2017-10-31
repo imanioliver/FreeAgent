844e4596acc49770f64a85f64ef492265cf7508c
Imani Oliver <imani.oliver@gmail.com>
2017-10-31 13:32:24 -0400

    yes

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
+
 //================================
 // User Schema
 //================================
