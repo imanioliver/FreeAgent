2b4a270fac7d48d37dc14e1a303d278c3433de2f
Imani Oliver <imani.oliver@gmail.com>
2017-10-31 13:44:43 -0400

    added new env var

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'pass the baton',
   // Database connection information
-  'database': process.env.MONGOLAB_URI,
+  'database': process.env.MONGOLAB_URI || process.env.MONGODB_URI,
   // 'database': 'mongodb://localhost:27017/free-agent',
   // Setting port for server
   'port': process.env.PORT || 3001
