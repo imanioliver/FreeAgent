1f7fdac9d21164c7162b558679daef0d72ff05d7
Imani Oliver <imani.oliver@gmail.com>
2017-11-01 14:39:46 -0400

    adds local database to allow api calls to function locally

--- config/main.js
+++ config/main.js
@@ -2,7 +2,15 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'pass the baton',
   // Database connection information
-  'database': process.env.MONGOLAB_URI || process.env.MONGODB_URI,
+
+
+  // var dbURI = 'mongodb://locationToMyMongoDB';
+  //
+  //   if (process.env.NODE_ENV === 'production') {
+  //       dbURI = process.env.MONGOLAB_URI;
+  //   }
+  //
+  'database': process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/free-agent',
   // 'database': 'mongodb://localhost:27017/free-agent',
   // Setting port for server
   'port': process.env.PORT || 3001
